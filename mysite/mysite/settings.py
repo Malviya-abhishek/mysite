055b4bfe8752c81c770d3ce055a43e8a6ecac83b
abhishek <abhishek.911malviya@gmail.com>
2020-10-05 00:15:45 +0530

    this is a update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8ay&h=)z*r&^1gbzqgjhl^usr&#pl33dw3xw0sn=504q)08rx0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
