37b01dd3be31365cfcbf0920aefb95732170125e
abhishek <abhishek.911malviya@gmail.com>
2020-10-05 11:27:50 +0530

    try this

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+def trial(request):
+	return HttpResponse("holly cow")
